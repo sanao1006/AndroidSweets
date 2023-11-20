7b2c27298703ac7fef681b484b0b586c132aa648
sanao1006 <naosak1006@gmail.com>
2023-11-20 15:33:40 +0900

    remove unnecessary files

--- app/src/main/java/app/sanao1006/androidsweets/MainActivity.kt
+++ app/src/main/java/app/sanao1006/androidsweets/MainActivity.kt
@@ -3,18 +3,12 @@ package app.sanao1006.androidsweets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-
-import app.sanao1006.androidsweets.ui.theme.AndroidSweetsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidSweetsTheme {
 
-            }
         }
     }
 }
