7aef80f0678b6397c0fa433bda85e2e76a28fb88
sanao1006 <naosak1006@gmail.com>
2023-11-21 09:54:05 +0900

    fix: 互換性の影響でビルドが通らなかったので修正

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -31,3 +31,10 @@ gradlePlugin {
         }
     }
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>()
+    .configureEach {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_17.toString()
+        }
+    }
