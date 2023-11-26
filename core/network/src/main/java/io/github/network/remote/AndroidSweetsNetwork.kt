0f11fe3ba32a3b1fd6d9eccd5b4d2fa2ebf959b3
sanao1006 <naosak1006@gmail.com>
2023-11-26 15:16:27 +0900

    feat: Retrofitのエラーハンドリングのための関数追加
    Fixed #33

--- core/network/src/main/java/io/github/network/remote/AndroidSweetsNetwork.kt
+++ core/network/src/main/java/io/github/network/remote/AndroidSweetsNetwork.kt
@@ -6,6 +6,7 @@ import io.github.network.model.ErrorIssue
 import io.github.network.model.ErrorMilestone
 import io.github.network.model.IssuesResponse
 import io.github.network.model.MilestoneResponse
+import okio.IOException
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -26,4 +27,30 @@ interface AndroidSweetsNetwork {
     suspend fun getIssuesForMilestone(
         @Query("milestone") milestoneId: String
     ): ApiResult<List<IssuesResponse>, ErrorIssue>
+
+    companion object {
+        suspend fun <A : Any> execute(run: suspend () -> ApiResult<A, ErrorMilestone>): A {
+            when (val res = run()) {
+                is ApiResult.Success -> return res.value
+                is ApiResult.Failure -> when (res) {
+                    is ApiResult.Failure.HttpFailure -> throw IOException(res.error?.message)
+                    is ApiResult.Failure.ApiFailure -> throw IOException(res.error?.message)
+                    is ApiResult.Failure.NetworkFailure -> throw res.error
+                    is ApiResult.Failure.UnknownFailure -> throw res.error
+                }
+            }
+        }
+
+        suspend fun <A : Any> execute(run: suspend () -> ApiResult<A, ErrorIssue>): A {
+            when (val res = run()) {
+                is ApiResult.Success -> return res.value
+                is ApiResult.Failure -> when (res) {
+                    is ApiResult.Failure.HttpFailure -> throw IOException(res.error?.message)
+                    is ApiResult.Failure.ApiFailure -> throw IOException(res.error?.message)
+                    is ApiResult.Failure.NetworkFailure -> throw res.error
+                    is ApiResult.Failure.UnknownFailure -> throw res.error
+                }
+            }
+        }
+    }
 }
