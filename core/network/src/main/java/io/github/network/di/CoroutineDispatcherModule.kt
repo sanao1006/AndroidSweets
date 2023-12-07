a3c44248d1eb6cc18ba94341c52141541fde1ee2
sanao1006 <naosak1006@gmail.com>
2023-12-07 21:58:11 +0900

    feat: CoroutineDispatcherをDIした
    withContextでCoroutineDispatcherを使うときにハードコードせずにDIしたものを使用するため。これによってテスト用威勢を高めることができる。
    
    Fixed #44

---
+++ core/network/src/main/java/io/github/network/di/CoroutineDispatcherModule.kt
@@ -0,0 +1,28 @@
+package io.github.network.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutineDispatcherModule {
+    @DefaultDispatcher
+    @Provides
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IODispatcher
+    @Provides
+    fun provideIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @UnconfinedDispatcher
+    @Provides
+    fun provideUnconfinedDispatcher(): CoroutineDispatcher = Dispatchers.Unconfined
+}
