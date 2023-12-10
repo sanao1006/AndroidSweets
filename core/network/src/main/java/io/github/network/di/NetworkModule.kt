35a52af81460c8656fc52be8ee03104d36835546
sanao1006 <naosak1006@gmail.com>
2023-12-10 10:11:48 +0900

    fix: 一旦コメントアウト

--- core/network/src/main/java/io/github/network/di/NetworkModule.kt
+++ core/network/src/main/java/io/github/network/di/NetworkModule.kt
@@ -1,15 +1,8 @@
 package io.github.network.di
 
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import io.github.network.remote.AndroidSweetsDataSource
-import io.github.network.remote.AndroidSweetsNetworkApi
-
-@Module
-@InstallIn(SingletonComponent::class)
-interface NetworkModule {
-    @Binds
-    fun binds(impl: AndroidSweetsNetworkApi): AndroidSweetsDataSource
-}
+// @Module
+// @InstallIn(SingletonComponent::class)
+// interface NetworkModule {
+//    @Binds
+//    fun binds(impl: AndroidSweetsNetworkApi): AndroidSweetsDataSource
+// }
