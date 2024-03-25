962c17df126143cd4b145b23a0fab92950591a48
sanao1006 <naosak1006@gmail.com>
2024-03-25 17:16:49 +0900

    change: nav

--- app/src/main/java/app/sanao1006/androidsweets/MainActivity.kt
+++ app/src/main/java/app/sanao1006/androidsweets/MainActivity.kt
@@ -3,11 +3,13 @@ package app.sanao1006.androidsweets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            SweetsApp()
         }
     }
 }
---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("androidsweets.android.application")
+    id("androidsweets.android.application.compose")
+    id("androidsweets.android.hilt")
+    id("androidsweets.spotless")
+}
+
+android {
+    namespace = "app.sanao1006.home"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
